9c762fe4389bcf3c61a8b9b18eba4080e87877e5
Alp179 <87341325+Alp179@users.noreply.github.com>
2026-01-07 10:55:12 +0000

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,18 +79,16 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon.ico', sizes: '48x48' },
     ],
     shortcut: '/favicon.ico',
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
     ],
-    other: [
-      { rel: 'icon', url: '/favicon.svg', type: 'image/svg+xml' }
-    ]
   },
   manifest: "/site.webmanifest",
   alternates: {

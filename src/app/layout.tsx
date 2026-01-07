8f48d0f185ccc0e6e22175edc66ed82e5f60c385
Alp179 <87341325+Alp179@users.noreply.github.com>
2026-01-07 16:05:27 +0000

    favicon bişeylers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,15 +79,15 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon.ico', sizes: '48x48' },
+      { url: 'https://rejectly.pro/favicon.svg', type: 'image/svg+xml' },
+      { url: 'https://rejectly.pro/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: 'https://rejectly.pro/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: 'https://rejectly.pro/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: 'https://rejectly.pro/favicon.ico', sizes: '48x48' },
     ],
-    shortcut: '/favicon.ico',
+    shortcut: 'https://rejectly.pro/favicon.ico',
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+      { url: 'https://rejectly.pro/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
     ],
   },
   manifest: "/site.webmanifest",
